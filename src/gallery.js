efead3f07e04ea88900eb1d2ec709ea839be1e5a
Jamie Forth <j.forth@gold.ac.uk>
2019-02-10 15:13:11 +0000

    check visual has a unique id

--- src/gallery.js
+++ src/gallery.js
@@ -6,11 +6,15 @@ export function Gallery() {
   // Add a new visual to the navigation bar.
   this.addVisual = function(vis) {
 
-    // Check that the object vis has a name.
+    // Check that the vis object has an id and name.
     if (!vis.hasOwnProperty('id')
         && !vis.hasOwnProperty('name')) {
-      // TODO: Check name is unique.
       alert('Make sure your visual has an id and name!');
+    }
+
+    // Check that the vis object has a unique id.
+    if (this.findVisIndex(vis.id) != null) {
+      alert(`Vis '${vis.name}' has a duplicate id: '${vis.id}'`);
     }
 
     this.visuals.push(vis);
@@ -27,26 +31,36 @@ export function Gallery() {
     }
   };
 
-  this.selectVisual = function(visId){
-    console.log('selectVisual:', visId);
-
-    // Search through the visuals looking for one with the id matching
-    // visId.
+  this.findVisIndex = function(visId) {
+    // Search through the visualisations looking for one with the id
+    // matching visId.
     for (var i = 0; i < this.visuals.length; i++) {
       if (this.visuals[i].id == visId) {
-        // If the visual has a deselect method run it.
-        if (this.selectedVisual != null
-           && this.selectedVisual.hasOwnProperty('destroy')) {
-          this.selectedVisual.destroy();
-        }
-        // Select the visual in the gallery.
-        this.selectedVisual = this.visuals[i];
+        return i;
+      }
+    }
 
-        // Initialise visualisation if necessary.
-        if (this.selectedVisual.hasOwnProperty('setup')) {
-          this.selectedVisual.setup();
-        }
+    // Visualisation not found.
+    return null;
+  };
+
+  this.selectVisual = function(visId){
+    var visIndex = this.findVisIndex(visId);
+
+    if (visIndex != null) {
+      // If the current visualisation has a deselect method run it.
+      if (this.selectedVisual != null
+          && this.selectedVisual.hasOwnProperty('destroy')) {
+        this.selectedVisual.destroy();
+      }
+      // Select the visualisation in the gallery.
+      this.selectedVisual = this.visuals[visIndex];
+
+      // Initialise visualisation if necessary.
+      if (this.selectedVisual.hasOwnProperty('setup')) {
+        this.selectedVisual.setup();
       }
     }
   };
+
 }
